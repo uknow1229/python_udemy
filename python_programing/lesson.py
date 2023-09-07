582050247245dd6384ddfc27d9ae993ffdaf0513
saya <uknow.love1229@gmail.com>
2023-09-07 21:02:35 +0900

    first commito

---
+++ python_programing/lesson.py
@@ -0,0 +1,21 @@
+# num = 1
+# name = 'Make'
+# is_ok = True
+
+# print(num, type(num ))
+# print(name, type(name))
+# print(is_ok, type(is_ok))
+
+# print('Hi', 'Mike', sep=',', end='.\n')
+
+# print(2 + 2)
+
+
+import math
+result = math.sqrt(5)
+print(result)
+
+y = math.log2(10)
+print(y)
+
+print
