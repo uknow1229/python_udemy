90945eb8932fded289707e0ee636f7adcd899c45
saya <uknow.love1229@gmail.com>
2023-09-11 15:28:27 +0900

    string

--- python_programing/lesson.py
+++ python_programing/lesson.py
@@ -8,14 +8,30 @@
 
 # print('Hi', 'Mike', sep=',', end='.\n')
 
-# print(2 + 2)
+# import math
+# result = math.sqrt(5)
+# print(result)
 
+# y = math.log2(10)
+# print(y)
 
-import math
-result = math.sqrt(5)
-print(result)
+print('hello')
+print("hello")
+print("I don't know")
+print('I don\'t know')
+print('say "I don\'t know"')
+print("say \"I don\'t know\"")
 
-y = math.log2(10)
-print(y)
+print('hello. \nHow are you?')
+# 改行したくない場合はrを先頭につける！
+print(r'C:\name\name')
 
-print
+# 複数行ある場合はこっちの方が見やすく改行できる！(\は空白行を出さないため)
+print("""\
+line1
+line2
+line3\
+""")
+
+# 文字列に演算子を使うこともできる！
+print('Hi.' * 3 + 'Mike.')
