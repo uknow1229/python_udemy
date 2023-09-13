5043f362de6f8f0822aff6cae0791c02a1131df4
saya <uknow.love1229@gmail.com>
2023-09-13 15:44:34 +0900

    section4_tuple

--- python_programing/section4_list.py
+++ python_programing/section4_list.py
@@ -271,3 +271,41 @@ print(X)
 # 4373915784
 # ['p', 'b']
 # ['p', 'b']
+
+# リストの使い所
+seat = []
+min = 0
+max = 5
+min <= len(seat) < max
+# True
+
+seat.append('p')
+min <= len(seat) < max
+# True
+
+len(seat)
+#1
+
+seat.append('p')
+min <= len(seat) < max
+# True
+
+len(seat)
+# 2
+seat.append('p')
+seat.append('p')
+min <= len(seat) < max
+# True
+
+seat.append('p')
+min <= len(seat) < max
+# False
+
+len(seat)
+# 5
+
+seat.pop(0)
+# 'p'
+
+min <= len(seat) < max
+# True
