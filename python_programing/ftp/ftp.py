e634204b185fa7a6c049890e4949e863c80a7104
saya <uknow.love1229@gmail.com>
2023-10-24 23:56:09 +0900

    ftp

--- python_programing/ftp/ftp.py
+++ python_programing/ftp/ftp.py
@@ -1,5 +1,27 @@
 from ftplib import FTP
 from logging import getLogger
+import os
+
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpblib.servers import FTPServer
+
+
+def setup_ftp_server():
+    authorizer = DummyAuthorizer()
+    authorizer.add_user("username", "password", "/path/to/your/directory", perm="elradfm")
+
+    handler = FTPHandler
+    handler.authorizer = authorizer
+
+    handler.directory = "/path/to/your/directory"
+
+    server = FTPServer(("127.0.0.1", 21), handler)
+    server.serve_forever()
+
+if __name__ == "__main__":
+    setup_ftp_server()
 
 # ログ出力設定
 logger = getLogger("FTP Test")
