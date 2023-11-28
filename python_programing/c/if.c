42a6cac60cf5aaa001a991cff0124469c703515c
saya <uknow.love1229@gmail.com>
2023-11-28 23:53:51 +0900

    if_c

---
+++ python_programing/c/if.c
@@ -0,0 +1,28 @@
+int main(void){
+
+  int a = 15;
+  int b = 2;
+  int c = 0;
+
+  if(a<10){
+    printf("under 10,");
+  }else if(a){// 10<=a<15
+
+  }
+  else{
+    printf("upper 10.");
+  }
+
+  int i = 0;
+
+  while(i<100){
+    printf("Hello\n");
+    i = i + 1;
+  }
+
+  for(i=0; i<100; i++){
+    printf("Hello\n");
+  }
+
+  return 0;
+}
